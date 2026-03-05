5bdeed7893c0469aaa3028bf4ad0dc898376bb8c
kunal sable <carrierhub41@gmail.com>
2026-03-05 13:44:53 +0530

    create a .env file in frontend

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "http://localhost:5000";
+// config.js
+
+export const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://employee-management-system-eu3o.onrender.com";
